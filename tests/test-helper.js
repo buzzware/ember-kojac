363c08f6a76c21e4d3555f69df5ff15c6499f151
Gary McGhee <gary@buzzware.com.au>
2018-08-03 17:44:24 +0800

    install ember-cli-mocha

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,3 +1,8 @@
+import resolver from './helpers/resolver';
+import { setResolver } from 'ember-mocha';
+
+setResolver(resolver);
+
 import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
